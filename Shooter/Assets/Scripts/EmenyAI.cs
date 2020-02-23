29d85e01510f7a769d1924c3231b727cfbca5632
GuillermoM <guillemg15@gmail.com>
2020-02-23 20:26:18 +0100

    0.3.0 Shooting
    Setting up the life of the enemies and creating the system of the raycast when shooting.

---
+++ Shooter/Assets/Scripts/EmenyAI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EmenyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5;
+
+    NavMeshAgent navMeshAgent;
+    float disctanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        disctanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (disctanceToTarget <= chaseRange)//Calcular diferencia de distancia entre el jugador y el enemigo
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if(disctanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if(disctanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + "Esta Atacando" + target.name);
+    }
+
+    void OnDrawGizmosSelected() //Parar ver el rango de seguimiento
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
